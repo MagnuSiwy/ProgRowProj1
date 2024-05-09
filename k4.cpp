cc0bb8cfe871984617c23df940126a3eea4e8abc
Tymoteusz Jagła <jagtymxd@gmail.com>
2024-05-09 19:16:08 +0200

    modify k4 but still not working

--- k4.cpp
+++ k4.cpp
@@ -1,59 +1,58 @@
 #include <math.h>
 #include <omp.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-int main() 
-{
-    long int m = 2, n = 10000000;
-    clock_t ppstart, ppstop;
-    double pswtime, pewtime;
+int main() {
+  long int m = 2, n = 10000000;
+  clock_t ppstart, ppstop;
+  double pswtime, pewtime;
 
-    bool *result = (bool *)malloc((n - m + 1) * sizeof(bool));
-    memset(result, true, (n - m + 1) * sizeof(bool));
+  bool *result = (bool *)malloc((n - m + 1) * sizeof(bool));
+  memset(result, true, (n - m + 1) * sizeof(bool));
 
-    bool *primeArray = (bool *)malloc((sqrt(n) + 1) * sizeof(bool));
-    memset(primeArray, true, (sqrt(n) + 1) * sizeof(bool));
+  bool *primeArray = (bool *)malloc((sqrt(n) + 1) * sizeof(bool));
+  memset(primeArray, true, (sqrt(n) + 1) * sizeof(bool));
 
-    pswtime = omp_get_wtime();
-    ppstart = clock();
+  pswtime = omp_get_wtime();
+  ppstart = clock();
 
-    for (int i = 2; i*i*i*i <= n; i++) 
-    {
-        if (primeArray[i] == true) 
-        {
-            for (int j = i*i; j*j <= n; j+=i) 
-            {
-                primeArray[j] = false;
-            }
-        }
+  for (int i = 2; i * i * i * i <= n; i++) {
+    if (primeArray[i] == true) {
+      for (int j = i * i; j * j <= n; j += i) {
+        primeArray[j] = false;
+      }
     }
+  }
 
-    #pragma omp parallel for
-    for (int i = 2; i*i <= n; i++)
-    {
-        if (primeArray[i]) 
-        {
-            int firstMultiple = (m / i);
+#pragma omp parallel for
+  for (int i = 2; i * i <= n; i++) {
+    if (primeArray[i]) {
+      int firstMultiple = (m / i);
 
-            if (firstMultiple <= 1) 
-            {
-                firstMultiple = i + i;
-            }
-            else if (m % i) 
-            { 
-                firstMultiple = (firstMultiple * i) + i;
-            }
-            else 
-            {
-                firstMultiple = (firstMultiple * i);
-            }
+      if (firstMultiple <= 1) {
+        firstMultiple = i + i;
+      } else if (m % i) {
+        firstMultiple = (firstMultiple * i) + i;
+      } else {
+        firstMultiple = (firstMultiple * i);
+      }
 
-            for (int j = firstMultiple; j <= n; j+=i)
-            { 
-                result[j-m] = false;
-            }
-        }
+      for (int j = firstMultiple; j <= n; j += j) {
+        result[j - m] = false;
+      }
     }
+  }
+
+  ppstop = clock();
+  pewtime = omp_get_wtime();
+
+  printf("Sito równoległe funkcyjne bez lokalności dostępu do danych\n");
+  printf("Czas procesorow przetwarzania sekwencyjnego: %f sekund\n",
+         ((double)(ppstop - ppstart) / CLOCKS_PER_SEC));
+  printf("Czas trwania obliczen sekwencyjnych - wallclock: %f sekund\n",
+         pewtime - pswtime);
 }
