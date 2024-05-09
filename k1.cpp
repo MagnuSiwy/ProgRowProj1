a67bb68109cbf0735d88e4e674775618a88fa87d
Tymoteusz Jagła <jagtymxd@gmail.com>
2024-05-09 18:39:04 +0200

    add time measurement to k1

--- k1.cpp
+++ k1.cpp
@@ -1,46 +1,47 @@
-#include <iostream>
-#include <cmath>
-#include <cstring>
-#include <stdexcept>
-#include <cstdlib>
+#include <math.h>
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-int main(int argc, char* argv[])
-{
-    if(argc < 2)
-    {
-        throw std::invalid_argument("Not enough arguments");
+int main(int argc, char *argv[]) {
+  long int m = 2, n = 1000000;
+  clock_t spstart, spstop;
+  double sswtime, sewtime;
+
+  bool *result = (bool *)malloc((n - m + 1) * sizeof(bool));
+  memset(result, true, (n - m + 1) * sizeof(bool));
+
+  bool *primeArray = (bool *)malloc((sqrt(n) + 1) * sizeof(bool));
+  memset(primeArray, true, (sqrt(n) + 1) * sizeof(bool));
+
+  sswtime = omp_get_wtime();
+  spstart = clock();
+
+  for (int i = 2; i * i <= n; i++) {
+    for (int j = 2; j * j <= i; j++) {
+      if (primeArray[j] == true && i % j == 0) {
+        primeArray[i] = false;
+        break;
+      }
     }
+  }
 
-    int m = atoi(argv[0]);
-    int n = atoi(argv[1]);
-
-    bool* result = (bool*)malloc((n - m + 1) * sizeof(bool));
-    memset(result, true, (n - m + 1) * sizeof(bool));
-
-    bool* primeArray = (bool*)malloc((sqrt(n) + 1) * sizeof(bool));
-    memset(primeArray, true, (sqrt(n) + 1) * sizeof(bool));
-
-    for (int i = 2; i * i <= n; i++) 
-    {
-        for (int j = 2; j * j <= i; j++) 
-        {
-            if (primeArray[j] == true && i % j == 0) 
-            { 
-                primeArray[i] = false; 
-                break;
-            }
-        }
+  for (int i = m; i <= n; i++) {
+    for (int j = 2; j * j <= i; j++) {
+      if (primeArray[j] == true && i % j == 0) {
+        result[i - m] = false;
+        break;
+      }
     }
+  }
 
-    for (int i = m; i <= n; i++)
-    {
-        for (int j = 2; j * j <= i; j++)
-        {
-            if (primeArray[j] == true && i % j == 0) 
-            {
-                result[i - m] = false;
-                break;
-            }
-        }
-    }
+  spstop = clock();
+  sewtime = omp_get_wtime();
+  printf("Dzielenie sekwencyjne:\n");
+  printf("Czas procesorow przetwarzania sekwencyjnego: %f sekund\n",
+         ((double)(spstop - spstart) / CLOCKS_PER_SEC));
+  printf("Czas trwania obliczen sekwencyjnych - wallclock: %f sekund\n",
+         sewtime - sswtime);
 }
