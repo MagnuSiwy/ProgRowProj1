84ebdee5d57ec267246ddea07e58fb7a3061b481
Tymoteusz Jagła <jagtymxd@gmail.com>
2024-05-09 18:29:25 +0200

    fix k2 not compiling

--- k2.cpp
+++ k2.cpp
@@ -1,50 +1,36 @@
-#include <iostream>
-#include <cmath>
-#include <cstring>
-#include <stdexcept>
-#include <cstdlib>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char* argv[])
-{
-    if(argc < 2)
-    {
-        throw std::invalid_argument("Not enough arguments");
+int main(int argc, char *argv[]) {
+  int m = atoi(argv[0]);
+  int n = atoi(argv[1]);
+
+  bool *result = (bool *)malloc((n - m + 1) * sizeof(bool));
+  memset(result, true, (n - m + 1) * sizeof(bool));
+
+  bool *primeArray = (bool *)malloc((sqrt(n) + 1) * sizeof(bool));
+  memset(primeArray, true, (sqrt(n) + 1) * sizeof(bool));
+
+  for (int i = 2; i * i <= n; i++) {
+    for (int j = 2; j * j <= i; j++) {
+      if (primeArray[j] == true && i % j == 0) {
+        primeArray[i] = false;
+        break;
+      }
     }
+  }
 
-    int m = atoi(argv[0]);
-    int n = atoi(argv[1]);
-    
-    bool* result = (bool*)malloc((n - m + 1) * sizeof(bool));
-    memset(result, true, (n - m + 1) * sizeof(bool));
-
-    bool* primeArray = (bool*)malloc((sqrt(n) + 1) * sizeof(bool));
-    memset(primeArray, true, (sqrt(n) + 1) * sizeof(bool));
-
-    for (int i = 2; i * i <= n; i++) 
-    {
-        for (int j = 2; j * j <= i; j++) 
-        {
-            if (primeArray[j] == true && i % j == 0) 
-            { 
-                primeArray[i] = false; 
-                break;
-            }
+#pragma omp parallel
+  {
+#pragma omp for
+    for (int i = m; i <= n; i++) {
+      for (int j = 2; j * j <= i; j++) {
+        if (primeArray[j] == true && i % j == 0) {
+          result[i - m] = false;
+          break;
         }
+      }
     }
-
-    #pragma omp parallel
-    {
-        #pragma omp for ? Jaki podział pracy ?
-        for (int i = m; i <= n; i++)
-        {
-            for (int j = 2; j * j <= i; j++)
-            {
-                if (primeArray[j] == true && i % j == 0) 
-                { 
-                    result[i - m] = false;
-                    break;
-                }
-            }
-        }
-    }
+  }
 }
