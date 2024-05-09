cca8b4a3d6b35c6a1b6de9ca7730e21821d31334
Tymoteusz Jagła <jagtymxd@gmail.com>
2024-05-09 18:45:09 +0200

    add time measurement to k2

--- k2.cpp
+++ k2.cpp
@@ -1,16 +1,23 @@
 #include <math.h>
+#include <omp.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 int main(int argc, char *argv[]) {
-  int m = atoi(argv[0]);
-  int n = atoi(argv[1]);
+  long int m = 2, n = 1000000;
+  clock_t ppstart, ppstop;
+  double pswtime, pewtime;
 
   bool *result = (bool *)malloc((n - m + 1) * sizeof(bool));
   memset(result, true, (n - m + 1) * sizeof(bool));
 
   bool *primeArray = (bool *)malloc((sqrt(n) + 1) * sizeof(bool));
   memset(primeArray, true, (sqrt(n) + 1) * sizeof(bool));
+
+  pswtime = omp_get_wtime();
+  ppstart = clock();
 
   for (int i = 2; i * i <= n; i++) {
     for (int j = 2; j * j <= i; j++) {
@@ -33,4 +40,13 @@ int main(int argc, char *argv[]) {
       }
     }
   }
+
+  ppstop = clock();
+  pewtime = omp_get_wtime();
+
+  printf("Dzielenie rownolegle\n");
+  printf("Czas procesorow przetwarzania sekwencyjnego: %f sekund\n",
+         ((double)(ppstop - ppstart) / CLOCKS_PER_SEC));
+  printf("Czas trwania obliczen sekwencyjnych - wallclock: %f sekund\n",
+         pewtime - pswtime);
 }
